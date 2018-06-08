b652c506fdacf626b855e3b42291a8e7b1b1da35
aish-pc <aishwary.agnihotri@untroddenlabs.com>
2018-06-09 02:22:15 +0530

    code improved

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const https = require('https');
 var winston = require('winston');
 var CronJob = require('cron').CronJob;
-var logger = new(winston.Logger)({
+const logDir = 'log';
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const env = process.env.NODE_ENV || 'development';
+
+var logger = new (winston.Logger)({
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({
-            filename: './all-logs.log'
+        new (winston.transports.File)({
+            filename: `${logDir}/results.log`,
+            timestamp: tsFormat,
+            maxsize: 5 * 1000 * 1024,
+            maxFiles: 1000,
+            colorize: true,
+            level: env === 'development' ? 'debug' : 'info'
         })
 
     ]
@@ -28,7 +37,7 @@ let ipLogger = () => {
             debugger
             let a = data.split('SOURCE IP INFO:');
             let b = a[1].split('ADVANCED SOURCE IP INFO WITH PROXY/TOR/SUSPICIOUS CHECK:');
-            logger.warn((JSON.parse(b[0])).ip,(JSON.parse(b[0])).region); 
+            logger.warn((JSON.parse(b[0])).ip, (JSON.parse(b[0])).region);
             // console.log((JSON.parse(b[0])).ip);
 
         });
